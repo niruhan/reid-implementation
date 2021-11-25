91abd07299a297cf46947b48fb845df9107b75b7
niruhan <niruhanv@gmail.com>
2021-11-25 22:46:44 +0530

    Add simple_train_save_plot_save.py

---
+++ simple_train_save_plot_save.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+from simple_model import ft_net
+import matplotlib.pyplot as plt
+
+h, w = 256, 128
+data_dir = '/home/niruhan/Personal/paper/Market-1501-v15.09.15/pytorch'
+batchsize = 2
+num_epochs = 10
+use_gpu = torch.cuda.is_available()
+
+transform_train_list = [
+    transforms.Resize((h, w), interpolation=3),
+    transforms.Pad(10),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+]
+
+transform_val_list = [
+    transforms.Resize(size=(h, w), interpolation=3),  # Image.BICUBIC
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+]
+
+data_transforms = {
+    'train': transforms.Compose(transform_train_list),
+    'val': transforms.Compose(transform_val_list),
+}
+
+image_datasets = {}
+image_datasets['train'] = datasets.ImageFolder(os.path.join(data_dir, 'train'),
+                                               data_transforms['train'])
+image_datasets['val'] = datasets.ImageFolder(os.path.join(data_dir, 'val'),
+                                             data_transforms['val'])
+
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batchsize, shuffle=True, num_workers=8)
+               for x in ['train', 'val']}
+
+class_names = image_datasets['train'].classes
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+
+model = ft_net(len(class_names))
+criterion = nn.CrossEntropyLoss()
+
+lr = 0.05
+optim_name = optim.SGD
+ignored_params = list(map(id, model.classifier.parameters()))
+base_params = filter(lambda p: id(p) not in ignored_params, model.parameters())
+classifier_params = model.classifier.parameters()
+optimizer = optim_name([
+    {'params': base_params, 'lr': 0.1 * lr},
+    {'params': classifier_params, 'lr': lr}
+], weight_decay=5e-4, momentum=0.9, nesterov=True)
+
+y_loss = {'train': [], 'val': []}  # loss history
+y_err = {'train': [], 'val': []}
+
+x_epoch = []
+fig = plt.figure()
+ax0 = fig.add_subplot(121, title="loss")
+ax1 = fig.add_subplot(122, title="top1err")
+
+
+def draw_curve(current_epoch):
+    x_epoch.append(current_epoch)
+    ax0.plot(x_epoch, y_loss['train'], 'bo-', label='train')
+    ax0.plot(x_epoch, y_loss['val'], 'ro-', label='val')
+    ax1.plot(x_epoch, y_err['train'], 'bo-', label='train')
+    ax1.plot(x_epoch, y_err['val'], 'ro-', label='val')
+    if current_epoch == 0:
+        ax0.legend()
+        ax1.legend()
+    fig.savefig(os.path.join('./lossGraphs', 'train.jpg'))
+
+
+def save_network(network, epoch_label):
+    save_filename = 'net_%s.pth' % epoch_label
+    save_path = os.path.join('./savedModels', save_filename)
+    torch.save(network.cpu().state_dict(), save_path)
+    if torch.cuda.is_available():
+        network.cuda()
+
+
+for epoch in range(num_epochs):
+    print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+    print('-' * 10)
+    # Each epoch has a training and validation phase
+    for phase in ['train', 'val']:
+        if phase == 'train':
+            model.train(True)  # Set model to training mode
+        else:
+            model.train(False)  # Set model to evaluate mode
+
+        running_loss = 0.0
+        running_corrects = 0.0
+
+        count = 0
+        # Iterate over data.
+        for data in dataloaders[phase]:
+            if count > 10:
+                break
+
+            count = count + 1
+            # get a batch of inputs
+            inputs, labels = data
+            now_batch_size, c, h, w = inputs.shape
+            if now_batch_size < batchsize:  # skip the last batch
+                continue
+            # print(inputs.shape)
+            # wrap them in Variable, if gpu is used, we transform the data to cuda.
+            if use_gpu:
+                inputs = Variable(inputs.cuda())
+                labels = Variable(labels.cuda())
+            else:
+                inputs, labels = Variable(inputs), Variable(labels)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # -------- forward --------
+            outputs = model(inputs)
+            _, preds = torch.max(outputs.data, 1)
+            loss = criterion(outputs, labels)
+
+            del inputs
+
+            # -------- backward + optimize --------
+            # only if in training phase
+            if phase == 'train':
+                loss.backward()
+                optimizer.step()
+
+            # statistics
+            running_loss += loss.item() * now_batch_size
+            del loss
+            running_corrects += float(torch.sum(preds == labels.data))
+
+        epoch_loss = running_loss / dataset_sizes[phase]
+        epoch_acc = running_corrects / dataset_sizes[phase]
+
+        print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+        y_loss[phase].append(epoch_loss)
+        y_err[phase].append(1.0 - epoch_acc)
+
+        # deep copy the model
+        if phase == 'val':
+            last_model_wts = model.state_dict()
+            if epoch % 10 == 9:
+                save_network(model, epoch)
+            draw_curve(epoch)
